4bf0bd619c8aa0b8202e06a2403643c8215e66f7
Anton Sosnin <antsosnin@yandex.ru>
2025-05-28 22:17:30 +0300

    Add data parsing script

--- utils/parse_data.py
+++ utils/parse_data.py
@@ -1,44 +1,102 @@
 import os
-import requests
-from bs4 import BeautifulSoup
 import json
 
-headers = {'User-Agent': 'CoolBot/0.0 (https://example.org/coolbot/; coolbot@example.org)'}
+def extract_translation(start, end, translation):
+    """
+    Extracts translations from the given translation string.
+    """
+    beg = translation.find(start)
+    end = translation.find(end)
+    if start != -1:
+        return translation[beg + len(start):end].strip()
+    return ''
 
-data = []
+questions = []
 
-for url in ['https://en.wikipedia.org/wiki/Road_signs_in_Cyprus', 'https://en.wikipedia.org/wiki/Road_signs_in_Greece']:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    gallery_boxes = soup.find_all(class_='gallerybox')
+# Find all *.csv files in the current directory
+for filename in os.listdir('.'):
+    if filename.endswith('.csv'):
+        with open(filename, 'r') as file:
+            # Skip the header line
+            next(file)
+            # Parse "Question Number|Question|Choices|Correct Answer|Translation"
+            # into json:
+            # {
+            #    "question": string,
+            #    "translation": string,
+            #    "answers": [
+            #      {
+            #        "answer": string,
+            #        "correct": boolean,
+            #        "translation": string
+            #      }
+            #    ]
+            # }
+            for line in file:
+                line = line.encode('utf-8').decode('utf-8-sig').strip()
+                line = line.replace('"', '')
+                data = line.split('|')
 
-    if not os.path.exists('images'):
-        os.makedirs('images')
+                tr = data[4].strip()
+                translations = []
+                translations.append(tr.split(' Α) ')[0].strip())
+                translations.append(extract_translation('Α) ', ', Β) ', tr))
+                translations.append(extract_translation('Β) ', ', Γ) ', tr))
+                if ' Δ) ' in tr:
+                    translations.append(extract_translation('Γ) ', ', Δ) ', tr))
+                    translations.append(tr.split(' Δ) ')[1].strip())
+                elif ' Γ) ' in tr:
+                    translations.append(tr.split(' Γ) ')[1].strip())
 
-    for box in gallery_boxes:
-        session = requests.Session()
+                answer = data[2].strip()#.split(',')[i].strip().replace('Α) ', '').replace('Β) ', '').replace('Γ) ', '').replace('Δ) ', '')
+                answers = []
+                answers.append(extract_translation('Α) ', ', Β) ', answer))
+                if 'Γ) ' in answer:
+                    answers.append(extract_translation('Β) ', ', Γ) ', answer))
+                elif 'Β)' in answer:
+                    answers.append(answer.split('Β)')[1].strip())
 
-        svg_url = box.find(class_='mw-file-description')['href']
-        name = box.find(class_='gallerytext').get_text(strip=True)
+                if ' Δ) ' in answer:
+                    answers.append(extract_translation('Γ) ', ', Δ) ', answer))
+                    answers.append(answer.split(' Δ) ')[1].strip())
+                elif ' Γ) ' in answer:
+                    answers.append(answer.split(' Γ) ')[1].strip())
 
-        # Download the SVG file
-        svg_page_link = "https://en.wikipedia.org" + svg_url;
-        svg_response = session.get(svg_page_link)
-        svg_soup = BeautifulSoup(svg_response.text, 'html.parser')
-        original_file_link = "https:" + svg_soup.find(class_='internal')['href']
+                question = {
+                    "question": data[1].strip(),
+                    "translation": translations[0],
+                    "answers": [
+                        {
+                            "answer": answers[i].strip(),
+                            "correct": data[3].strip() == str(i),
+                            "translation": translations[i + 1] if i < len(translations) - 1 else ''
+                        }
+                        for i in range(len(answers))
+                    ]
+                }
 
-        print(f'Downloading {original_file_link}...')
-        svg_file_response = session.get(original_file_link, headers=headers)
-        print(f'Content: {svg_file_response.text[:16]}...')
-        svg_file = os.path.basename(svg_url).replace('File:', 'images/')
-        with open(f'./{svg_file}', 'wb') as f:
-            if svg_file_response.headers.get('content-type') == 'image/svg+xml':
-                f.write(svg_file_response.text.encode('utf-8'))
-            else:
-                f.write(svg_file_response.content)
+                # Translate and sort σωστό/λάθος
+                if len(question['answers']) == 2:
+                    true_false_answers = [{}, {}]
+                    for answer in question['answers']:
+                        answer_str = answer['answer'].strip().lower()
+                        if answer_str == 'σωστό':
+                            answer['answer'] = 'σωστό'
+                            answer['translation'] = 'верно'
+                            true_false_answers[0] = answer
+                        elif answer_str == 'λάθος':
+                            answer['answer'] = 'λάθος'
+                            answer['translation'] = 'неверно'
+                            true_false_answers[1] = answer
+                        question['answers'] = true_false_answers
 
-        data.append({'image': svg_file, 'name': name})
+                # Check answers size
+                if len(question['answers']) > 4:
+                    raise Exception(f"Too many answers for question: {question['question']}")
+
+                questions.append(question)
+                
 
 # Save the data to a JSON file
-with open('./data.json', 'w') as f:
-    json.dump(data, f, indent=2)
+with open('./data.json', 'w', encoding='utf-8') as f:
+    json.dump(questions, f, ensure_ascii=False, indent=2)
